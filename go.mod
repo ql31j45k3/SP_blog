cbc38f4bdeb8a12dc0dbe38b4aded47c0ad4753d
Michael Kao <bttb2018to2019@gmail.com>
2021-08-17 23:39:42 +0800

    feat: 新增 mongodb tool

--- go.mod
+++ go.mod
@@ -8,9 +8,12 @@ require (
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.2.0
+	github.com/pelletier/go-toml v1.7.0 // indirect
 	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.6.1
+	go.mongodb.org/mongo-driver v1.5.3 // indirect
 	go.uber.org/dig v1.10.0
+	golang.org/x/text v0.3.7 // indirect
 	gorm.io/driver/mysql v1.0.0
 	gorm.io/gorm v1.9.19
 )
--- internal/utils/tools/const.go
+++ internal/utils/tools/const.go
@@ -4,3 +4,11 @@ const (
 	DefaultNotAssignInt = -1
 	StatusEnable        = 1
 )
+
+const (
+	ErrorFormatFind         = "mongosql.Find [ Collection: %s, condition: %v, Error: %w ]"
+	ErrorFormatCursorDecode = "cursor.Decode [ Collection: %s, condition: %v, Error: %w ]"
+
+	ErrorFormatAggregate             = "mongo.Collection Aggregate [ Collection: %s, pipeline: %v, Error: %w ]"
+	ErrorFormatAggregateCursorDecode = "mongo.Collection Aggregate cursor.Decode [ Collection: %s, pipeline: %v, Error: %w ]"
+)
