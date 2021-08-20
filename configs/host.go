a1428d0a25592a55fef60cbee03170b0df405468
Michael Kao <bttb2018to2019@gmail.com>
2021-08-20 15:10:57 +0800

    fix: configs race condition
    同時執行 reload and GetSPBlogApisHost
    會有並發問題，需用 sync.RWMutex 處理 reace condition

--- configs/host.go
+++ configs/host.go
@@ -1,6 +1,10 @@
 package configs
 
-import "github.com/spf13/viper"
+import (
+	"sync"
+
+	"github.com/spf13/viper"
+)
 
 func newConfigHost() *configHost {
 	config := &configHost{
@@ -13,13 +17,21 @@ func newConfigHost() *configHost {
 type configHost struct {
 	_ struct{}
 
+	sync.RWMutex
+
 	spBlogApisHost string
 }
 
 func (c *configHost) reload() {
+	c.Lock()
+	defer c.Unlock()
+
 	c.spBlogApisHost = ":" + viper.GetString("host.sp_blog_apis_host")
 }
 
 func (c *configHost) GetSPBlogApisHost() string {
+	c.RLock()
+	defer c.RUnlock()
+
 	return c.spBlogApisHost
 }
